91aa02effc2cc7a166642b6a8cf81d02b58a6a17
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-03-29 10:49:10 +0500

    make tests, add yml

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,30 +1,9 @@
 from django.test import TestCase
-from rest_framework.test import APIClient, APITestCase
-
-from logistic.models import Stock
-from stocks_products.settings import REST_FRAMEWORK
-
-
-answer = {
-    "id": 1,
-    "title": "Помидор",
-    "description": "Самые сочные и ароматные помидорки"
-}
+from rest_framework.test import APIClient
 
 
 class TestView(TestCase):
     def test_product_view(self):
         client = APIClient()
-        response = client.get('api/v1/products/1/')
+        response = client.get('/api/v1/products/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(Stock.objects.get('id'), 1)
-
-# второй вариант через APITestCase
-
-# class TestView(APITestCase):
-#     def test_product_view(self):
-#         url = 'api/v1/products/1/'
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code == 200)
-
-
