c39d2d5ef603b01c82bb836a332187443c8b8795
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-03-31 11:42:31 +0500

    add view

--- logistic/views.py
+++ logistic/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -18,3 +20,7 @@ class StockViewSet(ModelViewSet):
     filterset_fields = ['address', 'products']
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['address', 'product']
+
+
+def hello(request):
+    return HttpResponse('OK, status 200')
--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
+        'NAME': os.path.join(BASE_DIR, os.getenv('DB_NAME'))
         # 'HOST': os.getenv('DB_HOST'),
         # 'PORT': os.getenv('DB_PORT'),
         # 'USER': os.getenv('DB_USER'),
