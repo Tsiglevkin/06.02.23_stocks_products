a3029fd6987e904fd08fb5e9f1eaaec42e1e8e67
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-03-31 11:15:15 +0500

    change settings and .env

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST'),
+        # 'PORT': os.getenv('DB_PORT'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
